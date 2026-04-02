f53573e9e455dd45f6fdbb3f53c2a6dbf9fc5967
Ilia Choly <ilia.choly@gmail.com>
2026-04-02 03:34:46 +0000

    Fix migration numbering and remove stale jira_webhook migration
    Migration 016_jira_webhook.sql was never committed to master. Replaced
    017_atlassian_webhook_secret.sql (which tried to rename a non-existent
    column) with 016_atlassian_webhook_secret.sql that adds the column directly.

--- internal/store/sql/migrations/017_atlassian_webhook_secret.sql
+++
@@ -1,7 +0,0 @@
--- +goose Up
-
-ALTER TABLE orgs RENAME COLUMN jira_webhook_secret TO atlassian_webhook_secret;
-
--- +goose Down
-
-ALTER TABLE orgs RENAME COLUMN atlassian_webhook_secret TO jira_webhook_secret;
