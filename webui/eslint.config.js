08d90dd6723a6214d339e6ebad185db3c8d0cbbf
Ilia Choly <ilia.choly@gmail.com>
2026-05-23 08:42:27 -0400

    chore: fix linting errors

--- webui/eslint.config.js
+++ webui/eslint.config.js
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'src/gen']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
@@ -18,6 +18,12 @@ export default defineConfig([
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+    },
+    linterOptions: {
+      reportUnusedDisableDirectives: 'error',
+    },
+    rules: {
+      'react-refresh/only-export-components': 'off',
     },
   },
 ])
